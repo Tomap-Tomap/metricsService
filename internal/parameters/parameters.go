e3921b17244d396160bc8815b9c61bf9128b0aa8
Tomap-Tomap <timur.konoplev@yandex.ru>
2024-05-16 14:25:18 +0300

    add parse config from json

--- internal/parameters/parameters.go
+++ internal/parameters/parameters.go
@@ -2,23 +2,33 @@
 package parameters
 
 import (
+	"cmp"
+	"encoding/json"
 	"flag"
 	"os"
 	"strconv"
+
+	"github.com/DarkOmap/metricsService/internal/logger"
+	"go.uber.org/zap"
 )
 
 // AgentParameters contains parameters for agent.
 type AgentParameters struct {
-	ListenAddr     string
-	CryptoKeyPath  string
-	Key            string
-	ReportInterval uint
-	RateLimit      uint
-	PollInterval   uint
+	ListenAddr     string `json:"address"`
+	CryptoKeyPath  string `json:"crypto_key"`
+	Key            string `json:"key"`
+	ReportInterval uint   `json:"report_interval"`
+	RateLimit      uint   `json:"rate_limit"`
+	PollInterval   uint   `json:"poll_interval"`
 }
 
 // ParseFlagsAgent return agent's parameters from console or env.
 func ParseFlagsAgent() (p AgentParameters) {
+	var config string
+	if envConfig := os.Getenv("CONFIG"); envConfig != "" {
+		config = envConfig
+	}
+
 	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	f.StringVar(&p.ListenAddr, "a", "localhost:8080", "address and port to server")
 	f.StringVar(&p.CryptoKeyPath, "crypto-key", "", "path to public key")
@@ -26,7 +36,15 @@ func ParseFlagsAgent() (p AgentParameters) {
 	f.UintVar(&p.ReportInterval, "r", 10, "report interval")
 	f.UintVar(&p.PollInterval, "p", 2, "poll interval")
 	f.UintVar(&p.RateLimit, "l", 10, "rate limit")
+
+	if config == "" {
+		f.StringVar(&config, "c", "config.json", "path to agent configuration")
+		f.StringVar(&config, "config", "config.json", "path to agent configuration")
+	}
+
 	f.Parse(os.Args[1:])
+
+	parseAgentFromFile(f, &p, config)
 
 	if envCKP := os.Getenv("CRYPTO_KEY"); envCKP != "" {
 		p.CryptoKeyPath = envCKP
@@ -67,20 +85,75 @@ func ParseFlagsAgent() (p AgentParameters) {
 	return
 }
 
+func parseAgentFromFile(f *flag.FlagSet, p *AgentParameters, config string) {
+	if config == "" {
+		return
+	}
+
+	var jsonP AgentParameters
+
+	file, err := os.Open(config)
+
+	if err != nil {
+		logger.Log.Warn("Failed open config file. Config file will not read", zap.Error(err))
+		return
+	}
+
+	jd := json.NewDecoder(file)
+	err = jd.Decode(&jsonP)
+
+	if err != nil {
+		logger.Log.Warn("Failed decode config file. Config file will not read", zap.Error(err))
+		return
+	}
+
+	if p.ListenAddr == f.Lookup("a").DefValue {
+		p.ListenAddr = cmp.Or(jsonP.ListenAddr, p.ListenAddr)
+	}
+
+	if p.CryptoKeyPath == f.Lookup("crypto-key").DefValue {
+		p.CryptoKeyPath = cmp.Or(jsonP.CryptoKeyPath, p.CryptoKeyPath)
+	}
+
+	if p.Key == f.Lookup("k").DefValue {
+		p.Key = cmp.Or(jsonP.Key, p.Key)
+	}
+
+	ri, _ := strconv.ParseUint(f.Lookup("r").DefValue, 10, 64)
+	if p.ReportInterval == uint(ri) {
+		p.ReportInterval = cmp.Or(jsonP.ReportInterval, p.ReportInterval)
+	}
+
+	pi, _ := strconv.ParseUint(f.Lookup("p").DefValue, 10, 64)
+	if p.PollInterval == uint(pi) {
+		p.PollInterval = cmp.Or(jsonP.PollInterval, p.PollInterval)
+	}
+
+	rl, _ := strconv.ParseUint(f.Lookup("l").DefValue, 10, 64)
+	if p.RateLimit == uint(rl) {
+		p.RateLimit = cmp.Or(jsonP.RateLimit, p.RateLimit)
+	}
+}
+
 // ServerParameters contains parameters for server.
 type ServerParameters struct {
-	FlagRunAddr     string
-	FileStoragePath string
-	CryptoKeyPath   string
-	DataBaseDSN     string
-	Key             string
-	StoreInterval   uint
-	Restore         bool
-	RateLimit       uint
+	FlagRunAddr     string `json:"address"`
+	FileStoragePath string `json:"file_storage_path"`
+	CryptoKeyPath   string `json:"crypto_key"`
+	DataBaseDSN     string `json:"database_dsn"`
+	Key             string `json:"key"`
+	StoreInterval   uint   `json:"store_interval"`
+	Restore         bool   `json:"restore"`
+	RateLimit       uint   `json:"rate_limit"`
 }
 
 // ParseFlagsServer return server's parameters from console or env.
 func ParseFlagsServer() (p ServerParameters) {
+	var config string
+	if envConfig := os.Getenv("CONFIG"); envConfig != "" {
+		config = envConfig
+	}
+
 	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	f.StringVar(&p.FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	f.StringVar(&p.Key, "k", "", "hash key")
@@ -95,7 +168,15 @@ func ParseFlagsServer() (p ServerParameters) {
 	f.UintVar(&p.StoreInterval, "i", 300, "interval in seconds for save storage")
 	f.BoolVar(&p.Restore, "r", true, "flag for upload storage from file")
 	f.UintVar(&p.RateLimit, "l", 10, "rate limit")
+
+	if config == "" {
+		f.StringVar(&config, "c", "config.json", "path to server configuration")
+		f.StringVar(&config, "config", "config.json", "path to server configuration")
+	}
+
 	f.Parse(os.Args[1:])
+
+	parseServerFromFile(f, &p, config)
 
 	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
 		p.FlagRunAddr = envAddr
@@ -138,4 +219,62 @@ func ParseFlagsServer() (p ServerParameters) {
 	}
 
 	return
+}
+
+func parseServerFromFile(f *flag.FlagSet, p *ServerParameters, config string) {
+	if config == "" {
+		return
+	}
+
+	var jsonP ServerParameters
+
+	file, err := os.Open(config)
+
+	if err != nil {
+		logger.Log.Warn("Failed open config file. Config file will not read", zap.Error(err))
+		return
+	}
+
+	jd := json.NewDecoder(file)
+	err = jd.Decode(&jsonP)
+
+	if err != nil {
+		logger.Log.Warn("Failed decode config file. Config file will not read", zap.Error(err))
+		return
+	}
+
+	if p.FlagRunAddr == f.Lookup("a").DefValue {
+		p.FlagRunAddr = cmp.Or(jsonP.FlagRunAddr, p.FlagRunAddr)
+	}
+
+	if p.FileStoragePath == f.Lookup("f").DefValue {
+		p.FileStoragePath = cmp.Or(jsonP.FileStoragePath, p.FileStoragePath)
+	}
+
+	if p.CryptoKeyPath == f.Lookup("crypto-key").DefValue {
+		p.CryptoKeyPath = cmp.Or(jsonP.CryptoKeyPath, p.CryptoKeyPath)
+	}
+
+	if p.DataBaseDSN == f.Lookup("d").DefValue {
+		p.DataBaseDSN = cmp.Or(jsonP.DataBaseDSN, p.DataBaseDSN)
+	}
+
+	if p.Key == f.Lookup("k").DefValue {
+		p.Key = cmp.Or(jsonP.Key, p.Key)
+	}
+
+	si, _ := strconv.ParseUint(f.Lookup("i").DefValue, 10, 64)
+	if p.StoreInterval == uint(si) {
+		p.StoreInterval = cmp.Or(jsonP.StoreInterval, p.StoreInterval)
+	}
+
+	rl, _ := strconv.ParseUint(f.Lookup("l").DefValue, 10, 64)
+	if p.RateLimit == uint(rl) {
+		p.RateLimit = cmp.Or(jsonP.RateLimit, p.RateLimit)
+	}
+
+	restore, _ := strconv.ParseBool(f.Lookup("r").DefValue)
+	if p.Restore == restore {
+		p.Restore = cmp.Or(jsonP.Restore, p.Restore)
+	}
 }
